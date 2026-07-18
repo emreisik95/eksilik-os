84fff2f7b645329dbc96f9d48808c1f8760ae3a3
Emre Isik <emreisik@yandex.com>
2026-07-18 23:52:57 +0300

    Pin Swift package versions

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .executable(name: "EksilikCoreHarness", targets: ["EksilikCoreHarness"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "5.3.0"),
+        .package(url: "https://github.com/tid-kijyun/Kanna.git", exact: "5.3.0"),
     ],
     targets: [
         .executableTarget(
